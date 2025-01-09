d6617e6f4f255502ae36b23c4644d9832e2aaef7
shravanchinchkar <shravanchinchkarstar@gmail.com>
2025-01-09 16:17:38 +0530

    add frontend file to project

--- Backend/db/database.js
+++ Backend/db/database.js
@@ -1,11 +1,10 @@
 const mongoose=require("mongoose")
-const { boolean } = require("zod")
 
 mongoose.connect("mongodb+srv://shravanchinchkar:Shravan%40132610@shrav.wg75m.mongodb.net/todoApp")
 const todoSchema=mongoose.Schema({
     title:String,
     description:String,
-    complete:Boolean
+    completed:Boolean
 })
 
 const todo=mongoose.model('todoApp',todoSchema)
--- Backend/index.js
+++ Backend/index.js
@@ -15,12 +15,13 @@ app.post("/todo", async (req, res) => {
       message: "You gave a wrong input",
     });
   } else {
-    const ceratedTodo = await todo.create({
+    const createdTodo = await todo.create({
       title: payLoad.title,
       description: payLoad.description,
-      completed:false
+      completed: false,
     });
-    if (!createTodo) {
+    console.log("Created todo:-", createTodo);
+    if (!createdTodo) {
       return res.status(400).json({
         message: "Something went wrong!",
       });
@@ -41,30 +42,32 @@ app.get("/todos", async (req, res) => {
 });
 
 //Endpoint for marking a specific todo as completed
-app.put("/complete",async(req, res) => {
+app.put("/complete", async (req, res) => {
   const payLoad = req.body;
   const parsedPayLoad = updateTodo.safeParse(payLoad);
+
   if (!parsedPayLoad.success) {
     return res.status(411).json({
       message: "Invalid Id",
     });
-  }
-  else{
-    const updatedTodo=await todo.update({
-        _id:req.body.id //condition of what to update
-    },{
-        completed:true
-    })
-    console.log("Updated:-",updatedTodo)
-    if(!updatedTodo){
-        return res.status(400).json({
-            message:"Something went wrong!"
-        })
-    }
-    else{
-        return res.status(200).json({
-            message:"Todo marked as completed!"
-        })
+  } else {
+    const updatedTodo = await todo.updateOne(
+      {
+        _id: req.body.id, //condition of what to update
+      },
+      {
+        completed: true,
+      }
+    );
+    console.log("Updated:-", updatedTodo);
+    if (!updatedTodo) {
+      return res.status(400).json({
+        message: "Something went wrong!",
+      });
+    } else {
+      return res.status(200).json({
+        message: "Todo marked as completed!",
+      });
     }
   }
 });
